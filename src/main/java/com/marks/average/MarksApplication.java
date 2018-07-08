af002dcfdeaf1093f6c2318515b7ffe39d0a19f9
Unknown <klisarkk@gmail.com>
2018-07-08 01:10:08 +0100

    First real commit

---
+++ src/main/java/com/marks/average/MarksApplication.java
@@ -0,0 +1,12 @@
+package com.marks.average;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marks/average/MarksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marks.average;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
